cb92a02045aa92e98757b46a815056db9e25dacd
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-17 23:14:42 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -34,6 +34,7 @@ def play(songs)
   if user_input.to_i.is_a?(Numeric) and user_input.to_i <= songs.length and user_input.to_i >= 0
     puts "Playing #{songs[user_input.to_i - 1]}"
   elsif user_input.in?(songs)
+    puts "AAAAAAAAAAAA"
     puts "Playing #{songs[songs.index(user_input)]}"
   else
     puts "Invalid input, please try again"
