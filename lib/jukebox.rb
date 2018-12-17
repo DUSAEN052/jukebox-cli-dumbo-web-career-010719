45454171e0e4087c596a6a3847b571f1da8be20c
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-17 22:50:21 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,7 +31,7 @@ def play(songs)
   puts "Please enter a song name or number"
   user_input = gets.chomp
 
-  if user_input.to_i.is_a?(Numeric) and user_input <= songs.length and user_input >= 0
+  if user_input.to_i.is_a?(Numeric) and user_input.to_i <= songs.length and user_input.to_i >= 0
     puts "Playing #{songs[user_input - 1]}"
   elsif user_input.is_a?(String) and songs.include?(user_input)
     puts "Playing #{songs[songs.index(user_input)]}"
